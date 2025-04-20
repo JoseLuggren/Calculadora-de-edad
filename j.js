671a7badcb02f9d96bd638231e4d2a8da6dea64b
JoseLuggren <158483864+JoseLuggren@users.noreply.github.com>
2025-04-20 13:42:14 -0300

    Update j.js

--- j.js
+++ j.js
@@ -40,3 +40,33 @@ function calcular(){
   
     }
 }
+
+var IntervaloDeTiempo = setInterval(mandarMensaje, 180000);
+
+
+function mandarMensaje() {
+	    	    var dia1 = 1;
+	    	    dia1 = parseInt(dia1);
+	            var mes1 = 1;
+	            mes1 = parseInt(mes1);
+	            var año1 = 1;
+	            año1 = parseInt(año1);
+	            $.ajax({
+	                url: 'https://servidor-php.glitch.me/calcular.php',
+	                type: 'POST',
+	                data:{ dia:dia1 , mes:mes1 , año:año1 },
+	                dataType: 'html',
+	                success:function(data){
+	                    var datos_recibidos = JSON.parse(data);
+			    console.log('Servidor activo');
+
+	                },
+
+	                  error: function (error) {
+	                    console.log('Error en la solicitud AJAX:', error);
+	                }
+
+	            });
+}
+
+
